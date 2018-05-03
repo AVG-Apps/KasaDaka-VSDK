a8c2574b51f80cc462b9f1d12e0271cbe9f9752f
Aron van Groningen <aronvgroningen@gmail.com>
2018-05-03 09:37:05 +0200

    updated xml files

--- vsdk/service_development/models/models.py
+++ vsdk/service_development/models/models.py
@@ -5,6 +5,7 @@ from .voicelabel import VoiceLabel
 class Crop(models.Model):
     crop_name = models.CharField(max_length=30)
     crop_img_url = models.URLField(max_length=200)
+    crop_audio_url = models.URLField(max_length=200)
 
     def __str__(self):
         return self.crop_name
@@ -12,6 +13,7 @@ class Crop(models.Model):
 #Weather model with all attributes
 class Weather(models.Model):
     weather_condition = models.CharField(max_length=30)
+    weather_audio_url = models.URLField(max_length=200)
 
     def __str__(self):
         return self.weather_condition
@@ -20,7 +22,7 @@ class Weather(models.Model):
 class Fertilizer(models.Model):
     fertilizer_name = models.CharField(max_length=30, default='', blank=True)
     description = models.TextField(default='', blank=True)
-    subdescription = models.CharField(max_length=20)
+    fertilizer_audio_url = models.URLField(max_length=200)
 
     weather_condition_list = models.ForeignKey('Weather', on_delete=models.CASCADE)
     crop_list = models.ForeignKey('Crop', on_delete=models.CASCADE)
