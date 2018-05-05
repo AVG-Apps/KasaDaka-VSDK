c63b64b2a512857e159c02b35143d9ff88716016
Aron van Groningen <aronvgroningen@gmail.com>
2018-05-05 20:38:33 +0200

    improved files audio

--- vsdk/service_development/urls.py
+++ vsdk/service_development/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 app_name= 'service-development'
 urlpatterns = [
+    # urls of the voice application
     url(r'^$', views.index, name='index'),
     url(r'^choice/(?P<element_id>[0-9]+)/(?P<session_id>[0-9]+)$', views.choice, name='choice'),
     url(r'^message/(?P<element_id>[0-9]+)/(?P<session_id>[0-9]+)$', views.message_presentation, name='message-presentation'),
@@ -13,14 +14,15 @@ urlpatterns = [
     url(r'^language_select/(?P<session_id>[0-9]+)$', views.LanguageSelection.as_view(), name = 'language-selection'),
     url(r'^record/(?P<element_id>[0-9]+)/(?P<session_id>[0-9]+)$', views.record, name='record'),
 
+    # urls of the web interface
     url(r'^add/fertilizer/$', views.add_fertilizer, name='add_fertilizer'),
     url(r'^add/crop/$', views.add_crop, name='add_crop'),
     url(r'^add/weather/$', views.add_weather, name='add_weather'),
-
     url(r'^fertilizer/(?P<id>\d+)/edit/$', views.edit_fertilizer, name='edit_fertilizer'),
     url(r'^fertilizer/(?P<id>\d+)/$', views.fertilizer, name='fertilizer'),
     url(r'^fertilizer/', views.main_page, name='main_page'),
 
+    # urls of the own create xml files
     url(r'^voice_app/', views.voice_app, name='voice_app'),
 
 ]
